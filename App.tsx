680d029eba811684a6832a98c30e36f365b19ff8
Guilherme Santos <guuilhermessantos@gmail.com>
2022-08-02 21:21:53 -0300

    fix: dev mobile project

--- App.tsx
+++ App.tsx
@@ -1,11 +1,37 @@
+import 'react-native-gesture-handler';
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { theme } from './src/theme';
+import  Widget  from './src/components/Widget'
+import * as SplashScreen from 'expo-splash-screen';
+import { 
+  useFonts, 
+  Inter_400Regular, 
+  Inter_500Medium 
+} from '@expo-google-fonts/inter';
 
 export default function App() {
+  SplashScreen.preventAutoHideAsync();
+  const  [fontsLoaded] = useFonts({
+    Inter_400Regular, Inter_500Medium
+  });
+
+  if (!fontsLoaded) {
+    return null
+  }
+  SplashScreen.hideAsync();
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{
+      flex: 1,
+      backgroundColor: theme.colors.background
+    }}>
+      <StatusBar 
+        style="light" 
+        backgroundColor="transparent"
+        translucent
+      />
+      
+      <Widget />
     </View>
   );
 }
